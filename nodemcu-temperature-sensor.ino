bb455faf0662328e024bb7c14a538714edfee7d6
Mikael Strid <mikael@pixeldigitalbyra.se>
2018-09-22 18:00:02 +0200

    Read temperature and humidity and log to serial port

--- nodemcu-temperature-sensor.ino
+++ nodemcu-temperature-sensor.ino
@@ -1,10 +1,37 @@
+#include "DHT.h"
+
+#define DHTPIN 4 // = D2 on the NodeMCU
+#define DHTTYPE DHT22
+
+#define SENSOR_ID "temp-1"
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.setTimeout(2000);
+  while (!Serial) {}
+
+  Serial.print(SENSOR_ID);
+  Serial.print(" started");
+  Serial.print("------------------------------");
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  if (isnan(t) || isnan(t)) {
+    Serial.println("Failed to read temperature/humidity from sensor");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  
   delay(3000);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
 }
