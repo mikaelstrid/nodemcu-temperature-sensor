88d7322b26cf18219ce0397efab5bff14f4a3641
Mikael Strid <mikael@pixeldigitalbyra.se>
2018-09-22 18:16:34 +0200

    Added connect to Wi-Fi and simple HTTP GET

--- nodemcu-temperature-sensor.ino
+++ nodemcu-temperature-sensor.ino
@@ -1,9 +1,14 @@
+#include <ESP8266WiFi.h>
 #include "DHT.h"
 
 #define DHTPIN 4 // = D2 on the NodeMCU
 #define DHTTYPE DHT22
 
-#define SENSOR_ID "temp-1"
+const char* sensor_id = "temp-1";
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+
+const char* host = "www.example.com";
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -12,9 +17,18 @@ void setup() {
   Serial.setTimeout(2000);
   while (!Serial) {}
 
-  Serial.print(SENSOR_ID);
-  Serial.print(" started");
-  Serial.print("------------------------------");
+  Serial.printf("\n%s started\n", sensor_id);
+  Serial.println("------------------------------");
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");      
+  }
+  Serial.print(" connected ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -32,6 +46,36 @@ void loop() {
   Serial.print("Temperature: ");
   Serial.print(t);
   Serial.println(" *C ");
+
+  
+  WiFiClient client;
+  Serial.printf("\n[Connecting to %s ... ", host);
+  if (client.connect(host, 80))
+  {
+    Serial.println("connected]");
+    client.print(String("GET /") + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" +
+                 "\r\n"
+                );
+
+    // Serial.println("[Response:]");
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        String line = client.readStringUntil('\n');
+        // Serial.println(line);
+      }
+    }
+    client.stop();
+    Serial.println("\n[Disconnected]");
+  }
+  else 
+  {
+    Serial.println("connection failed!]");
+    client.stop();
+  }
   
   delay(3000);
 }
